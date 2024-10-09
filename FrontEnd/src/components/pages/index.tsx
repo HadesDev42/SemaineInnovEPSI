3851bb5ef0eaf992d1f7236eff39217c48c82e17
ThibaultBenuz <91091837+Haaycee@users.noreply.github.com>
2024-10-09 09:38:56 +0200

    Revert "Frontend"

--- FrontEnd/src/components/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./About";
-export * from "./Contact";
-export * from "./Home";
-export * from "./Services";
-export * from "./Signup";
-export * from "./Login";
-export * from "./Allergy";
